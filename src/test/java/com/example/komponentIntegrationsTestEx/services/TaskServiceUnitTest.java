3245ea9cfa5bbf29de4b12b090f5263a7c945c61
Caspar-Brantberger <3tomu24.branca@folkuniversitetet.nu>
2025-05-12 13:33:33 +0200

    UnitTest for task

--- src/test/java/com/example/komponentIntegrationsTestEx/services/TaskServiceUnitTest.java
+++ src/test/java/com/example/komponentIntegrationsTestEx/services/TaskServiceUnitTest.java
@@ -1,13 +1,20 @@
 package com.example.komponentIntegrationsTestEx.services;
 
+import com.example.komponentIntegrationsTestEx.models.Task;
+import com.example.komponentIntegrationsTestEx.models.User;
 import com.example.komponentIntegrationsTestEx.repositorys.TaskRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
 import static org.mockito.Mockito.verify;
 
 
@@ -29,5 +36,22 @@ public class TaskServiceUnitTest {
         taskService.deleteTask(taskId);
 
         verify(taskRepository, times(1)).deleteById(taskId);
+    }
+    @Test
+    void testGetTaskByIdReturnsTask () {
+        long taskId = 1L;
+
+        Task task = new Task();
+        task.setId(taskId);
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(task));
+
+        //act
+        Task result = taskService.getTaskById(1L);
+
+        //assert
+        assertEquals(1L, result.getId());
+        verify(taskRepository).findById(1L);
+
+
     }
 }
